2eae9f10888b0c235d03f6ab80cad24e6899209b
parul1994 <88855953+parul1994@users.noreply.github.com>
2024-02-11 15:30:38 +0530

    Update fb_login.java

--- src/test/java/facebook_register/fb_login.java
+++ src/test/java/facebook_register/fb_login.java
@@ -3,8 +3,7 @@ package facebook_register;
 public class fb_login {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hi my name is parul");
 	}
 
 }
